db2e3099da893000e9e62a6ffd9c1acadaf437a3
Michał Michalczewski <michalczewski.michal@gmail.com>
2017-05-22 21:51:26 +0200

    [task] Rounded button, thrown out unused navbar and added redirect

--- application_content/urls.py
+++ application_content/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+		url(r'^$', RedirectView.as_view(url='index')),
     url(r'^index$', views.index, name='index'),
     url(r'^profile$', views.profile, name='profile'),
     url(r'^settings$', views.settings, name='index'),
