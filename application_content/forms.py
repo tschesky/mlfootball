5e62576d401b142476ed126ae5a81cade623dfa8
Piotr Rząsa <piotrrek21@gmail.com>
2017-05-22 12:11:39 +0200

    [task] working registration. you can add new user now.

--- application_content/forms.py
+++ application_content/forms.py
@@ -1,5 +1,8 @@
 # _*_ coding: utf-8 _*_
+import re
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class RegistrationForm(forms.Form):
@@ -9,3 +12,21 @@ class RegistrationForm(forms.Form):
     password2 = forms.CharField(label="Repeat Password:", widget=forms.PasswordInput())
     phone = forms.CharField(label="Phone:", max_length=20, required=False)
     log_on = forms.BooleanField(label="Loging after registration:", required=False)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 == password2:
+            return password2
+        else:
+            raise forms.ValidationError("Hasła się różnią")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Dopuszczalne są tylko cyfry, litery angielskie i _")
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError("Taki użytkownik już istnieje")
