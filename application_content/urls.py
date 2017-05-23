cb6cdfde00154b2147b0a170a94d2123172e6896
Piotr Rząsa <piotrrek21@gmail.com>
2017-05-23 16:23:19 +0200

    [fix] little fixes with urls

--- application_content/urls.py
+++ application_content/urls.py
@@ -4,12 +4,12 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-		url(r'^$', RedirectView.as_view(url='index')),
+    url(r'^$', RedirectView.as_view(url='index')),
     url(r'^index$', views.index, name='index'),
     url(r'^news$', views.news, name='news'),
     url(r'^news$', views.index, name='index'),
     url(r'^profile$', views.profile, name='profile'),
-    url(r'^settings$', views.settings, name='index'),
-    url(r'^settings/password/$', views.password, name='index'),
+    url(r'^settings/$', views.settings, name='settings'),
+    url(r'^settings/password$', views.password, name='password'),
     url(r'^register/$', views.register_page, name='register_page'),
 ]
